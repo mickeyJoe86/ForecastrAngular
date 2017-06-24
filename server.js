623af42e89bb9df2cc6fbd171fdbebf7bf5f8415
Mike Ward <ward.michael51@gmail.com>
2017-06-24 11:46:03 -0400

    format changes

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ app.get('/', (req, res) => {
 	res.render('index');
 });
 
-app.get('/api/forecast', (req, res)=> {
+app.get('/api/forecast', (req, res) => {
 	request(`https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233`, (error, forecastResponse, forecastData) => {
 		if (!error && forecastResponse.statusCode == 200) {
-			res.writeHead(200, {"Content-Type": "application/json"});
+			res.writeHead(200, { "Content-Type": "application/json" });
 			res.end(forecastData);
 		}
 	});
 });
 
-app.listen(app.get('port'), function() {
-  console.log('App is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+	console.log('App is running on port', app.get('port'));
 });
