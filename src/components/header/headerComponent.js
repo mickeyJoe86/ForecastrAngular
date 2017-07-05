1995e6c21db40ebb79b9edb853bd8e01192e9eaa
Mike Ward <ward.michael51@gmail.com>
2017-07-05 19:22:40 -0400

    added spec for headerCtrl

--- src/components/header/headerComponent.js
+++ src/components/header/headerComponent.js
@@ -1,14 +1,15 @@
 angular.module('components.headerComponent', [])
-    .component('headernavComponent', {
+	.controller('HeaderComponentController', ['GelocationService', '$location', function (GelocationService, $location) {
+		var ctrl = this;
+		ctrl.name = "Forecastr";
+		ctrl.searchLocation = function () {
+			GelocationService.getCoordinates(ctrl.searchTerm).then(function (res) {
+				var latLong = res.results[0].geometry.location;
+				$location.url(latLong.lat + '/' + latLong.lng);
+			});
+		}
+	}])
+	.component('headernavComponent', {
 		templateUrl: './dist/components/header/headerView.html',
-		controller: ['GelocationService', '$location', function (GelocationService, $location) {
-			var ctrl = this;
-			ctrl.name = "Forecastr";
-			ctrl.searchLocation = function () {
-				GelocationService.getCoordinates(ctrl.searchTerm).then(function(res){
-					var latLong = res.results[0].geometry.location;
-					$location.url(latLong.lat + '/' + latLong.lng);
-				});
-			}
-		}]
+		controller: 'HeaderComponentController'
 	});
---
+++ tests/components/header/headerComponent.spec.js
@@ -0,0 +1,22 @@
+describe('HeaderComponentController', () => {
+	let $controller, HeaderComponentController, GelocationService, $location;
+
+	beforeEach(angular.mock.module('ui.router'));
+	beforeEach(angular.mock.module('components.headerComponent'));
+	beforeEach(angular.mock.module('services.geolocation'));
+
+
+	beforeEach(inject(function (_$controller_, _GelocationService_, _$location_) {
+		$controller = _$controller_;
+		GelocationService = _GelocationService_;
+		$location = _$location_;
+
+		HeaderComponentController = $controller('HeaderComponentController', { GelocationService: GelocationService, $location: $location });
+	}));
+
+	it('should exist', () => {
+		expect(HeaderComponentController).toBeDefined();
+	});
+
+	describe('searchLocation()', () => {});
+});
