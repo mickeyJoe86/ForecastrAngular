4f995786788484d14a64a2f1524a75d280397fdb
Mike Ward <ward.michael51@gmail.com>
2017-07-09 17:13:00 -0400

    testing es6 test for travis

--- tests/services/forecastService.spec.js
+++ tests/services/forecastService.spec.js
@@ -1,26 +1,26 @@
 describe('Forecast service', () => {
-	var ForecastService, $q, $httpBackend;
-	var RESPONSE_SUCCESS = {};
+	let ForecastService, $q, $httpBackend;
+	let RESPONSE_SUCCESS = {};
 	beforeEach(angular.mock.module('services.forecast'));
 
-	beforeEach(inject(function(_ForecastService_, _$q_, _$httpBackend_) {
+	beforeEach(inject((_ForecastService_, _$q_, _$httpBackend_) => {
 		ForecastService = _ForecastService_;
 		$q = _$q_;
 		$httpBackend = _$httpBackend_;
 	}));
 
-	it('should exist', function() {
+	it('should exist', () => {
 		expect(ForecastService).toBeDefined();
 	});
 
-	describe('getForecast()', function() {
-		var result;
-		var lat, lng;
-		beforeEach(function() {
+	describe('getForecast()', () => {
+		let result;
+		let lat, lng;
+		beforeEach(() => {
 			result = {};
 			spyOn(ForecastService, 'getForecast').and.callThrough();
 		});
-		it('should be defined', function() {
+		it('should be defined', () => {
 			expect(ForecastService.getForecast).toBeDefined();
 		});
 	});
