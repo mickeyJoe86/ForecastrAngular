553252933fa1a6a95cbccb226ba4bf484fc334f9
Mike Ward <ward.michael51@gmail.com>
2017-08-02 14:47:14 -0400

    added toolbar toggle

--- src/Config.ts
+++ src/Config.ts
@@ -1,6 +1,7 @@
-export const Config = ['$stateProvider', '$urlRouterProvider', (
+export const Config = ['$stateProvider', '$urlRouterProvider', '$mdThemingProvider', (
 	$stateProvider: angular.ui.IStateProvider,
-	$urlRouterProvider: angular.ui.IUrlRouterProvider) => {
+	$urlRouterProvider: angular.ui.IUrlRouterProvider,
+	$mdThemingProvider: any) => {
 
 	let helloState = {
 		name: 'home',
@@ -9,5 +10,13 @@ export const Config = ['$stateProvider', '$urlRouterProvider', (
 	}
 
 	$stateProvider.state(helloState);
-	$urlRouterProvider.otherwise('/38.9071923/-77.0368707');
+	$urlRouterProvider.otherwise('/38.9071923/-77.0368707');	
+
+	$mdThemingProvider.theme('default').primaryPalette('blue');
+	
+	var whiteMap = $mdThemingProvider.extendPalette('grey', { '500': '#ffffff', 'contrastDefaultColor': 'dark' });
+	var blackMap = $mdThemingProvider.extendPalette('grey', { '500': '#aaaaaa', 'contrastDefaultColor': 'dark' });
+	
+	$mdThemingProvider.definePalette('white', whiteMap);
+	$mdThemingProvider.definePalette('black', blackMap);
 }];
--- src/components/header.ts
+++ src/components/header.ts
@@ -1,19 +1,27 @@
 import { GeolocationService } from '../services/GeolocationService';
 
 const Header = {
-	template: `
-		<div layout="column" layout-fill>
-			<md-toolbar>
-				<div class="md-toolbar-tools">
-					<h2 md-truncate flex>Forecastr</h2>
-				</div>
-				<md-content>
-					<div id="main-containter">
-						<ui-view></ui-view>
-					</div>
-				</md-content>
-			</md-toolbar>
-		</div>
+	template: `		
+		<md-toolbar ng-show="!showSearch" class="md-whiteframe-1dp">
+			<div class="md-toolbar-tools">
+				<h2 md-truncate flex>Forecastr</h2>
+				<md-button ng-click="showSearch = !showSearch" md-colors="::{color: 'white'}">
+					<md-icon>search</md-icon>
+				</md-button>
+			</div>
+		</md-toolbar>	
+		<md-toolbar md-colors="::{background: 'white'}" ng-show="showSearch" class="md-whiteframe-1dp">
+			<div class="md-toolbar-tools">
+				<md-button ng-click="showSearch = !showSearch">
+					<md-icon md-colors="::{color: 'black'}">arrow_back</md-icon>
+				</md-button>
+				<md-input-container md-theme="input" flex>
+            		<label>&nbsp;</label>
+            		<input ng-model="search.who" placeholder="enter search">
+          		</md-input-container>
+				<md-button class="md-raised md-primary">Search</md-button>
+			</div>
+		</md-toolbar>	
 	`,
 	controller: class HeaderController {
 		name: string;
