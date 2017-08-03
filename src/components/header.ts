732ce5b097b2a18f7c064f95b69063ef3da16d5e
Mike Ward <ward.michael51@gmail.com>
2017-08-02 21:19:56 -0400

    header complete

--- src/components/header.ts
+++ src/components/header.ts
@@ -1,29 +1,29 @@
 import { GeolocationService } from '../services/GeolocationService';
 
 const Header = {
-	template: `		
-		<md-toolbar ng-show="!showSearch" class="md-whiteframe-1dp">
+	template: `
+		<md-toolbar ng-show="!$header.showSearch" class="md-whiteframe-1dp">
 			<div class="md-toolbar-tools">
 				<h2 md-truncate flex>Forecastr</h2>
-				<md-button ng-click="showSearch = !showSearch" md-colors="::{color: 'white'}">
+				<md-button ng-click="$header.showSearch = !$header.showSearch" md-colors="::{color: 'white'}">
 					<md-icon>search</md-icon>
 				</md-button>
 			</div>
-		</md-toolbar>	
-		<md-toolbar md-colors="::{background: 'white'}" ng-show="showSearch" class="md-whiteframe-1dp">
+		</md-toolbar>
+		<md-toolbar md-colors="::{background: 'white'}" ng-show="$header.showSearch" id="module-header-search" class="md-whiteframe-1dp">
 			<form ng-submit="$header.searchLocation()">
 				<div class="md-toolbar-tools">
-					<md-button ng-click="showSearch = !showSearch">
+					<md-button ng-click="$header.showSearch = !$header.showSearch">
 						<md-icon md-colors="::{color: 'black'}">arrow_back</md-icon>
-					</md-button>					
-					<md-input-container flex>						
+					</md-button>
+					<md-input-container flex class="input-container">
 						<label>&nbsp;</label>
-						<input placeholder="Search city, zip, or place" ng-model="$header.searchTerm" autocomplete="off">						
+						<input placeholder="Search city, zip, or place" ng-model="$header.searchTerm" autocomplete="off">
 					</md-input-container>
-					<md-button class="md-raised md-primary" type="submit">Search</md-button>				
+					<md-button class="md-raised md-primary" type="submit">Search</md-button>
 				</div>
 			</form>
-		</md-toolbar>	
+		</md-toolbar>
 	`,
 	controller: class HeaderController {
 		name: string;
